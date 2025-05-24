ca101cec1018f23a5f2b9d385513a1e555f1b913
McPuzzle <mcpazzle@gmail.com>
2025-05-24 14:03:15 +0300

    Update audio_separator.py

--- yt-midi-server/audio_separator.py
+++ yt-midi-server/audio_separator.py
@@ -1,5 +1,4 @@
 import subprocess
 
 def run_spleeter(input_path, output_path):
-    subprocess.run(['pip', 'install', 'spleeter==2.4.2', '--no-cache-dir'], check=True)
     subprocess.run(['spleeter', 'separate', '-i', input_path, '-o', output_path], check=True)
